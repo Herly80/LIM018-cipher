cd49fda126bea0e7374fa0a3d5ac2861fc737204
Herly <herlyyidam@gmail.com>
2022-07-05 10:24:03 -0500

    mi trabajo terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
 const cipher = {
-  // ...
-};
-
+  
+  encode : function (offset , string){ 
+   let result = "";
+    offset = parseInt(offset);
+    for (let i= 0; i<(string.length); i++) {
+      let code = string.charCodeAt(i);
+     
+      let enCodex = (code - 65 + offset) %26 + 65;
+    
+      result = result+String.fromCharCode(enCodex);
+     
+    }
+    
+      return result;
+  },
+  decode : function (offset , string) {
+    let result = "";
+    offset = parseInt(offset);
+    for (let i= 0; i<(string.length); i++) {
+      let code = string.charCodeAt(i);
+     
+      let enCodex = (code + 65 - offset) %26 + 65;
+    
+      result = result+String.fromCharCode(enCodex);
+     
+    }
+    
+      return result;
+  
+  }
+}
+console.log(cipher.encode(3,"ABCD"));
 export default cipher;
